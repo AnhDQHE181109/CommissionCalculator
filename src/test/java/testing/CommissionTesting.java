c3e3007d3a1e2fb912a7e1dec71763e8227040d2
f0sk9 <quannhhe181157@fpt.edu.vn>
2024-07-01 22:56:07 +0700

    finished testcase row 3 and after

--- src/test/java/testing/CommissionTesting.java
+++ src/test/java/testing/CommissionTesting.java
@@ -31,5 +31,30 @@ public class CommissionTesting {
         assertEquals("$0", main.CalculateCommission(false, "standard", 10000, false));
         assertEquals("$0", main.CalculateCommission(false, "standard", 1200, false));
         assertEquals("$0", main.CalculateCommission(false, "standard", 1000, false));
+
+        //third column and after (wuan)
+        assertEquals("$0", main.CalculateCommission(false, "bonus", 1000, true));
+        assertEquals("$0", main.CalculateCommission(false, "bonus", 1200,true));
+        assertEquals("$0", main.CalculateCommission(false, "bonus", 10000, true));
+        assertEquals("$0", main.CalculateCommission(false, "bonus", 10200, true));
+
+        assertEquals("10% commission <= $1000", main.CalculateCommission(false, "bonus", 1000, false));
+
+        assertEquals("$75", main.CalculateCommission(true, "bonus", 1200, false));
+        assertEquals("$75", main.CalculateCommission(true, "bonus", 10000, false));
+        assertEquals("$75", main.CalculateCommission(true, "bonus", 10200, false));
+
+        assertEquals("$0", main.CalculateCommission(true, "standard", 1200, false));
+        assertEquals("$0", main.CalculateCommission(true, "standard", 1000, false));
+        assertEquals("$0", main.CalculateCommission(true, "standard", 10000, false));
+        assertEquals("$0", main.CalculateCommission(true, "standard", 10200, false));
+
+        assertEquals("$0", main.CalculateCommission(true, "standard", 10200, true));
+        assertEquals("$0", main.CalculateCommission(true, "standard", 10000, true));
+        assertEquals("$0", main.CalculateCommission(true, "standard", 1200, true));
+        assertEquals("$0", main.CalculateCommission(true, "standard", 1000, true));
+
+
+
     }
 }
